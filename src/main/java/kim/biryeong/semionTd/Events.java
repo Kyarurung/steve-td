ce149997169643ea42e5868163f5303e89470b27
biryeongtrain <biryeongtrain06@gmail.com>
2026-05-15 02:04:18 +0900

    refactor(event): relocate event initializer to separate class

---
+++ src/main/java/kim/biryeong/semionTd/Events.java
@@ -0,0 +1,28 @@
+package kim.biryeong.semiontd;
+
+import kim.biryeong.semiontd.game.SemionGameManager;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+
+public final class Events {
+
+    public static void initialize(SemionGameManager gameManager) {
+
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            gameManager.tick(server);
+            gameManager.tickStartupLobbyLoad(server);
+        });
+
+        ServerLifecycleEvents.SERVER_STARTED.register(gameManager::scheduleStartupLobbyLoad);
+        ServerLifecycleEvents.SERVER_STOPPING.register(server -> gameManager.shutdown());
+        ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+            gameManager.handlePlayerJoin(handler.getPlayer());
+        });
+
+    }
+
+    private Events() throws IllegalAccessException {
+        throw new IllegalAccessException("Utility Class");
+    }
+}
